d9522ae245fd171b466112720ab0faf5383b48bf
Stoyan24 <stoiansmilqnov24@gmail.com>
2019-03-08 21:57:43 +0200

    Up

---
+++ Pokemon/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad alias architecto deleniti dignissimos error facilis ipsa ipsam laboriosam laborum necessitatibus nihil odit qui repellendus, totam ullam unde. Pariatur, quisquam.</p>
+
+        </div>
+    )
+}
+
+export default About
---
+++ Pokemon/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad alias architecto deleniti dignissimos error facilis ipsa ipsam laboriosam laborum necessitatibus nihil odit qui repellendus, totam ullam unde. Pariatur, quisquam.</p>
+
+        </div>
+    )
+}
+
+export default Home
